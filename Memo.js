5d29a2ab478c3b6266f1ba711b7b597a83e3cf67
JUN\gurwn <gurwns232@naver.com>
2021-04-06 22:57:48 +0900

    MEMO
    #1 Naming convertion내용 추가

---
+++ Memo.js
@@ -0,0 +1,14 @@
+// #1 Naming Convertion
+var rate = 0.2;
+function calToMlg(tt){
+    var tot = 0;
+    // 마일리지 계산...
+    return tot;
+}
+
+//better
+// const BONUS_RATE = 0.2;
+function getTotalMileage(distanceList){ /* 네이밍 정확하게 다른사람이 보아도 구분할 수 있도록 */
+    let totalMileage = 0; /* var -> Global변수 선언이므로 지양 let사용 */
+    return totalMileage;
+}
