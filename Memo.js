4b1a2ed0582c09393d08786361306fe949d8b655
JUN <gurwns232@naver.com>
2021-04-08 15:57:07 +0900

    MEMO #4 spread operator

--- Memo.js
+++ Memo.js
@@ -1,44 +1,80 @@
-// #1 Naming Convertion
-var rate = 0.2;
-function calToMlg(tt){
-    var tot = 0;
-    // 마일리지 계산...
-    return tot;
-}
+// // #1 Naming Convertion
+// var rate = 0.2;
+// function calToMlg(tt){
+//     var tot = 0;
+//     // 마일리지 계산...
+//     return tot;
+// }
 
-//better
-// const BONUS_RATE = 0.2;
-function getTotalMileage(distanceList){ /* 네이밍 정확하게 다른사람이 보아도 구분할 수 있도록 */
-    let totalMileage = 0; /* var -> Global변수 선언이므로 지양 let사용 */
-    return totalMileage;
-}
+// //better
+// // const BONUS_RATE = 0.2;
+// function getTotalMileage(distanceList){ /* 네이밍 정확하게 다른사람이 보아도 구분할 수 있도록 */
+//     let totalMileage = 0; /* var -> Global변수 선언이므로 지양 let사용 */
+//     return totalMileage;
+// }
 
-// #2 template literal
+// // #2 template literal
+// // bad
+// function introduceMember(member){
+//     return(
+//         "이 멤버의 이름은 " + member.name + "이고 나이는 " +member.age+"이며 직업은 " +member.job 
+//     );
+// }
+
+// // better
+// function introduceMember(member){
+//     return `이 멤버의 이름은 ${member.name} 이고 나이는 ${member.age} 이며 직업은 ${member.job}`
+// }
+
+// // #3 destructuring !!!
+// //bad
+// /* const member = {
+//     name: "개복치",
+//     age: "24",
+//     job: "개발자"
+// } */
+// function introduceMember(member){
+//     return `이 멤버의 이름은 ${member.name} 이고 나이는 ${member.age} 이며 직업은 ${member.job}`
+// }
+
+// //better
+// function introduceMember(member){
+//     const {name, age, job} = member;
+//     return `이 멤버의 이름은 ${name} 이고 나이는 ${age} 이며 직업은 ${job}`
+// }
+
+// #4 spread operator
 // bad
-function introduceMember(member){
-    return(
-        "이 멤버의 이름은 " + member.name + "이고 나이는 " +member.age+"이며 직업은 " +member.job 
-    );
-}
+// const member = {
+//     name: "개복치",
+//     age: "24",
+//     job: "개발자"
+// }
+// const job = {
+//     task: "frontend",
+//     tool: "react",
+//     slary: 5000
+// }
+// /* member.task = job.task;
+// member.tool = job.tool;
+// member.slary = job.slary; */
 
-// better
-function introduceMember(member){
-    return `이 멤버의 이름은 ${member.name} 이고 나이는 ${member.age} 이며 직업은 ${member.job}`
-}
 
-// #3 destructuring !!!
-//bad
-const member = {
-    name: "개복치",
-    age: "24",
-    job: "개발자"
-}
-function introduceMember(member){
-    return `이 멤버의 이름은 ${member.name} 이고 나이는 ${member.age} 이며 직업은 ${member.job}`
-}
+// // Object.assign(member,job)
 
-//better
-function introduceMember(member){
-    const {name, age, job} = member;
-    return `이 멤버의 이름은 ${name} 이고 나이는 ${age} 이며 직업은 ${job}`
-}
+// // console.log(member);
+
+
+// //good
+// const memberJob = { ...member, ...job, area: "seoul" }
+
+// console.log(memberJob);
+
+// //array
+// const frontend = ["react", "vue", "angular"];
+// const backend = ["spring", "django", "node.js"];
+// const publising = ["html", "css", "javascript"];
+
+// const fullstack = [...frontend, ...backend, ...publising];
+
+// console.log(fullstack);
