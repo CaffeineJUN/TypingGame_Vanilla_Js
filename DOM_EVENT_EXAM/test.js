fa54488ced80a03b0b1540e845555d3689e2efed
JUN <gurwns232@naver.com>
2021-04-09 19:20:08 +0900

    #DOM_EVENT_EXAM test.js 추가 작성
    querySelector의 다양한 사용 메모

--- DOM_EVENT_EXAM/test.js
+++ DOM_EVENT_EXAM/test.js
@@ -1,14 +1,47 @@
-console.log(window);
+// console.log(window);
 
-// single element
-// const form = document.getElementById('todo-form');
-const form = document.querySelector("#todo-form");
-console.log(form);
+// // single element
+// // const form = document.getElementById('todo-form');
+// const form = document.querySelector("#todo-form");
+// console.log(form);
 
-// multiple element
-const items = document.querySelectorAll('.item');
-items.forEach((item) => console.log(item));
+// // multiple element
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
 
-console.log(document.getElementsByClassName('item'));
-// console.log(document.getElementsByTagName('li'));
+// console.log(document.getElementsByClassName('item'));
+// // console.log(document.getElementsByTagName('li'));
+
+const todos = document.querySelector('#todo_list');
+// // todos.remove();
+// // todos.style.background = 'green';
+// todos.lastElementChild.remove(); /* 마지막 */
+// todos.firstElementChild.textContent = 'Hello';
+// todos.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+
+
+
+const button = document.querySelector('.submit');
+const todoInput = document.querySelector('#todo_input');
+const msg = document.querySelector('#msg');
+button.addEventListener('click', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(todoInput.value === ''){
+        msg.style.display = 'block'
+        setTimeout(() => msg.style.display = 'none', 2000);
+        return;
+    }
+    // console.log('clicked!!!');
+    // e.target.style.color = 'red';
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(todoInput.value));
+    console.log(li);
+    li.classList.add('item');
+    todos.appendChild(li);
+    todoInput.value = '';
+}
 
