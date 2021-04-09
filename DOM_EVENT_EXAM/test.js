26397bf5144c6a3b0ca85d8287b420c1b9b21c67
JUN <gurwns232@naver.com>
2021-04-09 17:57:54 +0900

    #DOM_EVENT_EXAM multiple element작성
    querySelectorAll() / getElementsByClassName() ..

--- DOM_EVENT_EXAM/test.js
+++ DOM_EVENT_EXAM/test.js
@@ -3,7 +3,12 @@ console.log(window);
 // single element
 // const form = document.getElementById('todo-form');
 const form = document.querySelector("#todo-form");
-
 console.log(form);
 
 // multiple element
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
+
+console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
