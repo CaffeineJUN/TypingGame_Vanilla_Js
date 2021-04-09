4b7f5048daf4cce5877dbae6caf1ac3cda514b3c
JUN <gurwns232@naver.com>
2021-04-10 02:29:32 +0900

    #TypingGame main.js 작성
    addEventListener,countDown함수 작성 및 변수 작성 수정

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score');
 const timeDisplay = document.querySelector('.time');
+const button = document.querySelector('.button');
 
 /* ------------------변수------------------ */
 
@@ -25,4 +26,11 @@ function countDown(){
     timeDisplay.innerText = time;
 }
 
-setInterval(countDown,1000);
+// setInterval(countDown,1000);
+
+function buttonChange(text){
+    button.innerText = text;
+    text === "게임시작" ? button.classList.remove('loading') : button.classList.add('loading')
+}
+
+buttonChange('게임시작');
