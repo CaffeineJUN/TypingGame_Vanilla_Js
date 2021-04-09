825c7cd05ea5d449aeee79dca7b333733149384d
JUN <gurwns232@naver.com>
2021-04-09 17:51:01 +0900

    #DOM_EVENT_EXAM test.js single element작성
    getElementById() / querySelector

---
+++ DOM_EVENT_EXAM/test.js
@@ -0,0 +1,9 @@
+console.log(window);
+
+// single element
+// const form = document.getElementById('todo-form');
+const form = document.querySelector("#todo-form");
+
+console.log(form);
+
+// multiple element
